97e41c0afb55e03103412231fce2e044077ebf03
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-01-17 20:25:24 +0000

    added destinations

--- lib/travel2020/destination.rb
+++ lib/travel2020/destination.rb
@@ -11,3 +11,15 @@ class Travel2020::Destination
     destination_1.name = "Bhutan"
     destination_1.continent = "Asia" 
     destination_1.url = "https://www.lonelyplanet.com/bhutan" 
+    
+    destination_2 = self.all
+    destination_2.name = "England" 
+    destination_2.continent = "Europe" 
+    destination_2.url = "https://www.lonelyplanet.com/england"
+    
+    destination_3 = self.all
+    destination_3.name = "North Macedonia"
+    destination_3.continent = "Europe"
+    destination_3.url = "https://www.lonelyplanet.com/macedonia"
+    
+    
