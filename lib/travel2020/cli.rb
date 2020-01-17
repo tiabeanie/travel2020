cea6f6899ac37b027a4ef9bf7401d52e5f5402a5
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-01-17 21:52:59 +0000

    update cli

--- lib/travel2020/cli.rb
+++ lib/travel2020/cli.rb
@@ -9,6 +9,9 @@ class Travel2020::CLI
   def list_destinations
     puts "Top Ten Travel Destinations for 2020"
     @destination = Travel2020::Destination.all
+    @destination.each.with_index(1) do |destination, i|
+      puts "#{i}. #{destination.name} - #{destination.continent}"
+    end 
   end 
   
   def menu
@@ -16,14 +19,10 @@ class Travel2020::CLI
     while input != "exit"
       puts "Enter the number of the destination you're interested in. Type list to see the destinations again or type exit to leave"
       input = gets.strip.downcase
-      case input 
-      when "1"
-        puts "more info on destination 1"
-      when "2" 
-        puts "more info on destination 2"
-      when "3"
-        puts "more info on destination 3"
-      when "list"
+      if input.to_i > 0 
+        the_destination = @destination[input.t0_i-1]
+        puts "#{i}. #{the_destination.name} - #{the_destinationdestination.continent}"
+      elsif input == "list"
         list_destinations
       else 
         puts "That number is unavailable, type list to see the destinations again or exit to leave"
--- lib/travel2020/destination.rb
+++ lib/travel2020/destination.rb
@@ -2,28 +2,10 @@ class Travel2020::Destination
   attr_accessor :name, :continent, :url
   
   def self.all 
-    #puts <<-DOC 
-    #  1. Bhutan
-    #  2. England
-    #  3. North Macedonia
-    #  DOC
-    
-    destination_1 = self.new
-    destination_1.name = "Bhutan"
-    destination_1.continent = "Asia" 
-    destination_1.url = "https://www.lonelyplanet.com/bhutan" 
-    
-    destination_2 = self.new 
-    destination_2.name = "England" 
-    destination_2.continent = "Europe" 
-    destination_2.url = "https://www.lonelyplanet.com/england"
-    
-    destination_3 = self.new 
-    destination_3.name = "North Macedonia"
-    destination_3.continent = "Europe"
-    destination_3.url = "https://www.lonelyplanet.com/macedonia"
-    
-    [destination_1, destination_2, destination_3]
-    
+    self.scrape_destinations
+  end 
+  
+  def self.scrape_destinations  
+    destinations = []
   end 
 end 
