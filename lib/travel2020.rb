fcb0916627f1fa457bbd6cd8b377d0079340a5cf
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-01-17 18:36:59 +0000

    added cli

--- lib/travel2020.rb
+++ lib/travel2020.rb
@@ -1,6 +1,4 @@
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
+
 
 require_relative "./travel2020/version"
 require_relative './travel2020/destination'
--- lib/travel2020/cli.rb
+++ lib/travel2020/cli.rb
@@ -1,15 +1,36 @@
 class Travel2020::CLI 
   
   def call 
-    puts "Top Ten Travel Destinations for 2020"
     list_destinations
+    menu
+    goodbye
   end 
   
   def list_destinations
-    puts <<-DOC
+    puts "Top Ten Travel Destinations for 2020"
+    puts <<-DOC 
       1. Bhutan
       2. England
       3. North Macedonia 
     DOC
+  end 
+  
+  def menu
+    puts "Enter the number of the destination you're interested in or type exit to enter"
+    while input != "exit"
+      input = gets.strip.downcase
+      case input 
+      when "1"
+        puts "more info on destination 1"
+      when "2" 
+        puts "more info on destination 2"
+      when "3"
+        puts "more info on destination 3"
+      end 
+    end 
+  end
+  
+  def goodbye
+    puts "Farewell, and happy travels!"
   end 
 end 
