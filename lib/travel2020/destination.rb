87ee1735843a850d18b24ee4f7db8207cf485d27
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-01-19 22:55:24 +0000

    updated scraper

--- lib/travel2020/destination.rb
+++ lib/travel2020/destination.rb
@@ -10,7 +10,7 @@ class Travel2020::Destination
     url = "https://www.forbes.com/sites/duncanmadden/2019/10/22/the-lonely-planet-top-ten-tourism-countries-for-2020/#756101384cdd"
     unparsed_page = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
-    destinations << name = parsed_page.css('h3').text
+    destinations << name = parsed_page.css('h3').text.split(/\d.\s/)
     byebug
     destinations
   end 
